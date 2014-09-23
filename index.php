9807418dd93a30158cd43ffc17dda1653ccf9f6c
Travis Baker <baker.travis.w@gmail.com>
2014-09-23 01:31:51 -0600

    Adding new info for home page.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
    <title>Hello CS313 World!</title>
    
    <!-- Bootstrap CSS -->
-   <link href="css/bootstrap.min.css" rel="stylesheet" />
+   <link href="/php/css/bootstrap.min.css" rel="stylesheet" />
    
    
    
@@ -24,45 +24,14 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="#">Travis Baker</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
+        <li <?php if ($_SERVER['PHP_SELF'] == "/php/index.php") { echo "class='active'"; } ?> ><a href="/php/index.php">Home</a></li>
+        <li <?php if ($_SERVER['PHP_SELF'] == "/php/assignments/index.php") { echo "class='active'"; } ?> ><a href="/php/assignments/index.php">Assignments</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
@@ -77,7 +46,7 @@
         <div class="row">
           <section class="col-xs-12">
             <section>
-              <h2>Hello CS313</h2>
+              <h2>Hello CS313 People</h2>
                 <p>This is my home page.</p>
             </section>
 
@@ -87,7 +56,7 @@
    <!-- jQuery -->
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
    <!-- Bootstrap JavaScript (including jQuery plugins -->
-   <script src="js/bootstrap.min.js"></script>
+   <script src="/php/js/bootstrap.min.js"></script>
 
 </body>
 </html>
