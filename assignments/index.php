ce7df1e806dd26d5b05eed1ab892a3f1f3959e67
Travis Baker <baker.travis.w@gmail.com>
2014-09-23 01:35:23 -0600

    quick fixes

---
+++ assignments/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+   <meta charset="utf-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+   <title>Hello CS313 World!</title>
+   
+   <!-- Bootstrap CSS -->
+   <link href="/css/bootstrap.min.css" rel="stylesheet" />
+   
+   
+   
+
+</head>
+<body>
+
+<?php include '/templates/menu.php'; ?>
+
+   <!-- jQuery -->
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+   <!-- Bootstrap JavaScript (including jQuery plugins -->
+   <script src="/js/bootstrap.min.js"></script>
+
+</body>
+</html>
--- templates/menu.php
+++ templates/menu.php
@@ -1,18 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-   <meta charset="utf-8" />
-   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-   <title>Hello CS313 World!</title>
-   
-   <!-- Bootstrap CSS -->
-   <link href="/css/bootstrap.min.css" rel="stylesheet" />
-   
-   
-   
-
-</head>
-<body>
+<?php echo "This is a test" ?>
 
 <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
@@ -36,27 +22,3 @@
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-
-<section class='container'>
-    <hgroup>
-      <h1>Hello World</h1>
-    </hgroup>
-
-
-        <div class="row">
-          <section class="col-xs-12">
-            <section>
-              <h2>Hello CS313 People</h2>
-                <p>This is my home page.</p>
-            </section>
-
-          </section>
-</section>
-
-   <!-- jQuery -->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-   <!-- Bootstrap JavaScript (including jQuery plugins -->
-   <script src="/js/bootstrap.min.js"></script>
-
-</body>
-</html>
